ad543b534257b653ad2be632088bdfef26e0a1a1
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-07-12 13:17:56 +0200

    Update test to version 4.0

--- tests/test_stock_supply_multiple.py
+++ tests/test_stock_supply_multiple.py
@@ -1,28 +1,17 @@
 #!/usr/bin/env python
-#This file is part of Tryton.  The COPYRIGHT file at the top level of
-#this repository contains the full copyright notices and license terms.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class StockSupplyMultipleTestCase(unittest.TestCase):
-    'Test StockSupplyMultiple module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('stock_supply_multiple')
-
-    def test0005views(self):
-        'Test views'
-        test_view('stock_supply_multiple')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class TestCase(ModuleTestCase):
+    'Test module'
+    module = 'stock_supply_multiple'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        StockSupplyMultipleTestCase))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
     return suite
